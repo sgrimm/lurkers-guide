5e782444ed4de216c4ab28b7d5946712d47ca9ce
Steven Grimm <koreth@midwinter.com>
2013-12-26 12:50:09 -0800

    Left out one perdir.py

---
+++ credits/perdir.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#
+# Per-directory values for /lurk/credits
+#
+def get_type():
+	return 'credits'
+
+def get_episode_names_file():
+	return '../internal/epnames'
+
+def get_page_name_prefix():
+	return 'Credits'
+
+def get_table_of_contents():
+	return ''
+
+def get_do_jpeg():
+	return 1
